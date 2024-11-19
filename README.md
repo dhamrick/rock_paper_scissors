efcfc51552ffcee87697d07e5e290a8fb9632682
Donovan Hamrick <donovan@blend.com>
2024-11-18 21:38:29 -0800

    Create functions to get players choices

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # rock_paper_scissors
 TOP JS fundamentals lesson to create a game of rock paper scissors
+
+
+
+
+Objective: Create a new function named getComputerChoice
+
+Psuedocode
+
+declare function getComputerChoice
+    define variable for computerChoice
+    assign computerChoice random number 0 - 2
+    get RPS selection from random number
+    assign RPS selection to computerChoice
+    return computerchoice
+
+declare function getHumanChoice
+    define variable for humanChoice
+    get humanChoice from user
+    get rps from humanChoice
+    return rps as humanChoice
+
--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*3);
+    switch(computerChoice) {
+        case 0:
+            return computerChoice = "scissors";
+        case 1:
+            return computerChoice = "rock";
+        case 2: 
+            return computerChoice = "paper";
+    }
+}
+
+function getHumanChoice() {
+    let humanChoice = parseInt(prompt("scissors (0), rock (1), or paper (2): ", "2"));
+
+    switch(humanChoice) {
+        case 0:
+            return humanChoice = "scissors";
+        case 1:
+            return humanChoice = "rock";
+        case 2: 
+            return humanChoice = "paper";
+    }
+
+}
+
+console.log(getComputerChoice()); 
+console.log(getHumanChoice());
