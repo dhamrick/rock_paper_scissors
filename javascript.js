2aab0de87aeeaf65ea175530176899b7da93f4d9
Donovan Hamrick <donovan@blend.com>
2025-01-20 13:07:08 -0800

    Remove playGame and  getHumanChoice funcitons

--- javascript.js
+++ javascript.js
@@ -20,43 +20,39 @@ function getComputerChoice() {
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    let round = 1;
+let round = 1;
 
-        console.log(`Round ${round}!`)
+    console.log(`Round ${round}!`)
 
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
+    const computerChoice = getComputerChoice();
+    
+    round++;        
 
-        playRound(humanChoice, computerChoice);
-        round++;        
-
-        function playRound(humanChoice, computerChoice) {
-            if (humanChoice === computerChoice) {
-                console.log(`Tie! Play again! Score is player ${humanScore} and computer ${computerScore}!`);
-            } else if (
-                //define all human player winning conditions
-                humanChoice === 'scissors' && computerChoice === 'paper' ||
-                humanChoice === 'rock' && computerChoice === 'scissors' ||
-                humanChoice === 'paper' && computerChoice === 'rock'
-            ) {
-                humanScore++;
-                console.log(`You won! ${humanChoice} beats ${computerChoice}! Score is now: Player ${humanScore} and Computer ${computerScore}!`);
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log(`Tie! Play again! Score is player ${humanScore} and computer ${computerScore}!`);
+        } else if (
+            //define all human player winning conditions
+            humanChoice === 'scissors' && computerChoice === 'paper' ||
+            humanChoice === 'rock' && computerChoice === 'scissors' ||
+            humanChoice === 'paper' && computerChoice === 'rock'
+        ) {
+            humanScore++;
+            console.log(`You won! ${humanChoice} beats ${computerChoice}! Score is now: Player ${humanScore} and Computer ${computerScore}!`);
+    
+        }else{
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}! Score is now: Player ${humanScore} and Computer ${computerScore}!`);
         
-            }else{
-                computerScore++;
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}! Score is now: Player ${humanScore} and Computer ${computerScore}!`);
-            
-            }
-        }  
+        }
+    }  
 
-    console.log(`Finale Score: Player ${humanScore}, Computer ${computerScore}`);
-    if(humanScore > computerScore) {
-        console.log("Congratulations! You won!");
-    } else {
-        console.log("The computer won! Better luck next time!");
-    }
+console.log(`Finale Score: Player ${humanScore}, Computer ${computerScore}`);
+if(humanScore > computerScore) {
+    console.log("Congratulations! You won!");
+} else {
+    console.log("The computer won! Better luck next time!");
 }
